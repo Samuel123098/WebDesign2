349125c10f44008a5fb8047414987b2a36c7edd1
Samuel123098 <S4muel2018@hotmail.com>
2025-01-11 20:26:11 +0000

    First1

---
+++ Script.js
@@ -0,0 +1,8 @@
+// Script.js
+
+// This function will run when the document is fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Document is fully loaded and parsed');
+    
+    // Your code here
+});
