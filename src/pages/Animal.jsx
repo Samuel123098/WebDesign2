ec37ded54446af1b91f0561c8fdac186914d3d8d
Samuel123098 <S4muel2018@hotmail.com>
2025-01-18 21:46:53 +0000

    design

--- src/pages/Animal.jsx
+++ src/pages/Animal.jsx
@@ -2,22 +2,11 @@ import Navi from "../components/HeaderSivenav.jsx";
 import "./css/Animal.modual.css"
 
 export default function Animal() {
-    const styles = {
-        main: {
-          backgroundColor: "#000000",
-          width: "100%",
-        },
-        inputText: {
-          padding: "10px",
-          color: "red",
-        },
-      };
+     document.body.style.backgroundColor = "black"
     return (
         <>
-            <div className="main" style={styles.main}></div>
             <Navi />
             <h1>Animal</h1>
-            <div className="main" style={styles.main}></div>
         </>
     );
 }
