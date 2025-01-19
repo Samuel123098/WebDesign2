675a58465c76c9eddc3bcccff5ca1b8a1d9ab84d
Samuel123098 <S4muel2018@hotmail.com>
2025-01-19 01:44:48 +0000

    AnImal Page

--- src/pages/Animal.jsx
+++ src/pages/Animal.jsx
@@ -2,11 +2,33 @@ import Navi from "../components/HeaderSivenav.jsx";
 import "./css/Animal.modual.css"
 
 export default function Animal() {
-     document.body.style.backgroundColor = "black"
+     document.body.style.backgroundColor = "#191919"
     return (
         <>
             <Navi />
-            <h1>Animal</h1>
+            <div className="Animals">
+                <div className="Animalcolumn">
+                    <img src="src/assets/pictures/Ted.jpeg" height={"auto"} width={"300px"} ></img>
+                        <h1>Ted</h1>
+                        <h2>Owner: Sam (me)</h2>
+                </div>
+                <div className="Animalcolumn">
+                    <img src="src/assets/pictures/Bruce.jpeg" height={"auto"} width={"300px"} ></img>
+                        <h1>Bruce</h1>
+                        <h2>Owner: Chloe</h2>
+                </div>
+                <div className="Animalcolumn">
+                    <img src="src/assets/pictures/Loki.png" height={"auto"} width={"300px"} ></img>
+                        <h1>Loki</h1>
+                        <h2>Owner: Orrin</h2>
+                </div>
+                <div className="Animalcolumn">
+                    <img src="src/assets/pictures/Buddy.png" height={"auto"} width={"300px"} ></img>
+                        <h1>Buddy</h1>
+                        <h2>Owner: Tyler</h2>
+                </div>
+            </div>
+
         </>
     );
 }
